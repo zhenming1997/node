ffb99b6848652bd6217794035d7086c27a12e6c3
甄铭 <1976868977@qq.com>
2020-07-21 14:20:41 +0800

    安装Element-ui,配置router为login

--- vue.config.js
+++ vue.config.js
@@ -41,7 +41,7 @@ module.exports = {
       }
     },
     // 启用 CSS modules for all css / pre-processor files.
-    requireModuleExtension: false
+    requireModuleExtension: true
   },
   // use thread-loader for babel & TS in production build
   // enabled by default if the machine has more than 1 cores
